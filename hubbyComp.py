6ed21994dc319059d545bf16efab2f40c10596f9
Kasey Lawrence <kasey.lawrence@gmail.com>
2023-08-05 06:42:28 -0500

    Update and including sequences.db sqlite db

--- hubbyComp.py
+++ hubbyComp.py
@@ -1,11 +1,15 @@
 import customtkinter
 import string
+import sqlite3
+
+db  = sqlite3.connect('sequences.db')
+cursor = db.cursor()
 
 customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("dark-blue")
 
 root = customtkinter.CTk()
-root.geometry("400x400")
+root.geometry("400x600")
 root.title("Hubby Comp")
 
 def revComp():
@@ -14,8 +18,40 @@ def revComp():
     replace_chars = "TGCA"
     tab = str.maketrans(old_chars, replace_chars)
     revcomp_result = sequence.translate(tab)[::-1]
+    query = cursor.execute('select * from indexes where i5_sequence like ?;', (sequence,)).fetchall()
+    revComp_query = cursor.execute('select * from indexes where i5_sequence like?;', (revcomp_result,)).fetchall()
+    if query:
+        if len(query) > 1:
+            messageLabel.configure(text="Original Index found in multiple i5s")
+        
+        else:
+            messageLabel.configure(text="Original Index found in 1 i5")
+        for uid,iid,i7seq,i5seq,kitname,manuf,indlen in query:
+            #print(f"{row['i5_sequence']} found in {row['index_group']} ")
+            message = f"{i5seq} found in {kitname}"
+            messageLabel.configure(text=message)
+        
+        #print(f"{query[0][3]} from {query[0][4]}")
+    
+    
+
+
+    if revComp_query:
+        if len(revComp_query) > 1:
+            messageLabel.configure(text="RevComp Sequence found in multiple i5s")
+
+        else:
+            messageLabel.configure(text="RevComp Sequence found")
+        for uid,iid,i7seq,i5seq,kitname,manuf,indlen in revComp_query:
+            messageLabel.configure(text=f"{i5seq} found in {kitname}")
+
+    if not query and not revComp_query:
+        messageLabel.configure(text='No Sequences found in database. Database might be incomplete')
+
+    #print (query)
     resultTextbox.delete("1.0", "end")
     resultTextbox.insert("1.0", revcomp_result)
+    
 
 frame = customtkinter.CTkFrame(master=root,)
 frame.pack(pady=20, padx=60, fill="both", expand=True)
@@ -37,5 +73,12 @@ resultLabel = customtkinter.CTkLabel(master=frame, text="Reverse Complement Sequ
 resultLabel.pack(pady=12, padx=10)
 resultTextbox = customtkinter.CTkTextbox(master=frame, width=300, height=2)
 resultTextbox.pack(pady=12, padx = 10)
+
+logLabel = customtkinter.CTkLabel(master=frame, text = "Log:")
+logLabel.pack(pady=12,padx=12)
+messageLabel = customtkinter.CTkLabel(master=frame, text = "")
+messageLabel.pack(pady=12,padx=12)
+
+
 
 root.mainloop()
